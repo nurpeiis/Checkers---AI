e4bb3a15e53aceadfe515f52811ca067ff718e7e
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-02-25 20:25:28 +0400

    Changes in checking the move and makefile created with g++ version

--- Board.cpp
+++ Board.cpp
@@ -69,162 +69,174 @@ MoveType Board::checkMove(position start, position end, Player player)
 {
     int xDiff = abs(start.second - end.second); //you can move either to left or right- doesnt really matter
     int yDiff = end.first - start.first;
-    MoveType move;
-    State state = NONE;
-    if ((player == Player::WHITEE) && (state == State::WHITE))
+    MoveType moveResult;
+
+    if(board.at(end).getState() != NONE){
+        cout<<"WHAT ARE YOU DOING?? The cell is occupied"<<endl;
+        moveResult=PROHIBITED;
+      }
+    else if (board.at(start).getState()==BLACK)
     {
         if ((board.at(end).getState() == NONE) && (xDiff == 1) && (yDiff == 1))
         {
-            if (end.second == 7)
-            {
-                JustCrowned = true;
-                move = USUAL;
+            if (end.first == 7)
+            {   JustCrowned = true;
+                moveResult = USUAL;
             }
             else
-            {
-                JustCrowned = false;
-                move = USUAL;
+            {   JustCrowned = false;
+                moveResult = USUAL;
             }
         }
-        else if ((board.at(end).getState() == NONE) && (xDiff == 2) && (yDiff == 2) && CheckIfValidJump(start, end))
+       else if ((board.at(end).getState() == NONE) && (xDiff == 2) && (yDiff == 2))
         {
             if (end.first == 7)
-            {
-                JustCrowned = true;
-                move = COMBAT;
+            {   JustCrowned = true;
+                moveResult = COMBAT;
             }
             else
             {
                 JustCrowned = false;
-                move = COMBAT;
+                moveResult = COMBAT;
             }
         }
-        else
-        {
-            return (move = PROHIBITED);
-        }
+
+
     }
-    else if ((player == Player::WHITEE) && (state == State::WHITEKING))
+    else if (board.at(start).getState()==BLACKKING)
     {
         if ((board.at(end).getState() == NONE) && (xDiff == 1) && ((yDiff == 1) || (yDiff == -1)))
-        {
-            JustCrowned = false;
-            move = USUAL;
+        {   JustCrowned = false;
+
+            moveResult = USUAL;
         }
         else if ((board.at(end).getState() == NONE) && ((xDiff == 2) && (yDiff == 2)) && CheckIfValidJump(start, end) == 1)
         {
             JustCrowned = false;
-            move = COMBAT;
+            moveResult = COMBAT;
         }
         else
         {
-            move = PROHIBITED;
+            moveResult = PROHIBITED;
         }
-    } //end of if WhiteKing
-    else if ((player == Player::BLACKK) && (state == State::BLACK))
+    }
+    else if (board.at(start).getState()==WHITE)
     {
         if ((board.at(end).getState() == NONE) && (xDiff == 1) && ((yDiff == -1)))
         {
-            if (end.second == 0)
+            if (end.first == 0)
             {
                 JustCrowned = true;
-                move = USUAL;
+                moveResult = USUAL;
             }
             else
             {
                 JustCrowned = false;
-                move = USUAL;
+                moveResult = USUAL;
             }
         }
-        else if ((board.at(end).getState() == NONE) && (xDiff == 1) && (yDiff == -2) && CheckIfValidJump(start, end) == 1)
+        else if ((board.at(end).getState() == NONE) && (xDiff == 2) && (yDiff == -2))
         {
-            if (end.second == 0)
+            if (end.first == 0)
             {
                 JustCrowned = true;
-                move = COMBAT;
+                moveResult = COMBAT;
+
             }
-            else
+          else
             {
                 JustCrowned = false;
-                move = COMBAT;
+                moveResult = COMBAT;
+
             }
         }
         else
         {
-            move = PROHIBITED;
+            moveResult = PROHIBITED;
         }
     }
 
-    else if ((player == Player::BLACKK) && (state == State::BLACKKING))
+    else if (board.at(start).getState()==WHITEKING)
     {
-        if ((board.at(end).getState() == NONE) && ((xDiff == 1)) && ((yDiff == 1) || (yDiff == -1)))
-            JustCrowned = false;
-        move = COMBAT;
+    if ((board.at(end).getState() == NONE) && ((xDiff == 1)) && ((yDiff == 1) || (yDiff == -1))){
+
+        JustCrowned = false;
+        moveResult = COMBAT;
     }
     else if ((board.at(end).getState() == NONE) && (xDiff == 1) && ((yDiff == -2) || (yDiff == 2)) && (CheckIfValidJump(start, end) == 1))
     {
         JustCrowned = false;
-        move = COMBAT;
+        moveResult = COMBAT;
+
     }
-    else
-    {
-        move = PROHIBITED;
-    }
-    return move;
+  }
+
+      else {
+        moveResult = PROHIBITED;
+      }
+    return moveResult;
 }
+
 
 MoveType Board::move(position start, position end, Player player)
 {
     MoveType moveResult = checkMove(start, end, player);
-    if (moveResult == USUAL)
+
+   if (moveResult == USUAL)
     {
         if (JustCrowned == false)
-            board.at(end).setState(board.at(start).getState());
+        board.at(end).setState(board.at(start).getState());
         board.at(start).setState(NONE);
-        //S
-        if (JustCrowned == true)
+
+        if (JustCrowned == true){
             if (board.at(start).getState() == WHITE)
             {
                 board.at(end).setState(WHITEKING);
                 board.at(start).setState(NONE);
-                //S
             }
             else
             {
-                board.at(end).setState(BLACKKING);
+                board.at(end).setState(WHITE);
                 board.at(start).setState(NONE);
-                //S
+
             }
     }
-    else if (moveResult == COMBAT)
+  }
+
+     if (moveResult == COMBAT)
     {
+      position middlePos((start.first + end.first) / 2, (start.second + end.second) / 2);
         if (JustCrowned == false)
-            board.at(end).setState(board.at(start).getState());
+            {
+
+        board.at(end).setState(board.at(start).getState());
+        board.at(middlePos).setState(NONE);
         board.at(start).setState(NONE);
-    
+        }
         if (JustCrowned == true)
             if (board.at(start).getState() == WHITE)
             {
                 board.at(end).setState(WHITEKING);
+                board.at(middlePos).setState(NONE);
                 board.at(start).setState(NONE);
-                //S
+
             }
             else
             {
                 board.at(end).setState(BLACKKING);
+                board.at(middlePos).setState(NONE);
                 board.at(start).setState(NONE);
-                //S
+
             }
-        position middlePos((start.first + end.first) / 2, (start.second + end.second) / 2);
-        board.at(middlePos).setState(NONE);
-        //S
-        cout << "Oh the other player jumped your piece";
+        cout << "Congrats! You ate the other player\n";
     }
     else if (moveResult == PROHIBITED)
+
     {
-        cout << "This move is not allowed. Try again :(";
+        cout << "This move is not allowed. Try again :(\n";
     }
     return moveResult;
+
 }
 
 bool Board::CheckIfValidJump(position start, position end)
@@ -234,15 +246,15 @@ bool Board::CheckIfValidJump(position start, position end)
     position check(CombatRow, CombatCol);
     if ((((board.at(end).getState() == BLACKKING) || (board.at(end).getState() == BLACK)) && ((board.at(check).getState() == WHITEKING) || (board.at(check).getState() == WHITE))))
     {
-        return true;
+        return 1;
     }
     else if ((((board.at(end).getState() == WHITEKING) || (board.at(end).getState() == WHITE)) && ((board.at(check).getState() == BLACKKING) || (board.at(check).getState() == BLACK))))
     {
-        return true;
+        return 1;
     }
     else
     {
-        return false;
+        return 0;
     }
 }
 Unordered_map Board::getBoard(){
--- makefile
+++ makefile
@@ -1,10 +1,14 @@
-fonbook: main.o hashMap.o trieName.o
-	g++ -o fonbook hashMap.o trieName.o main.o 
-hashMap.o: hashMap.h hashMap.cpp
-	g++ -c hashMap.cpp
-trieName.o: trieName.h trieName.cpp
-	g++ -c trieName.cpp
+checkers: main.o Cell.o Board.o Game.o IOstream.o
+	g++ -o checkers main.o Cell.o Board.o Game.o IOstream.o
+Cell.o: Cell.h Cell.cpp
+	g++ -c Cell.cpp -std=c++11
+Game.o: Game.h Game.cpp
+	g++ -c Game.cpp -std=c++11
+Board.o: Board.h Board.cpp
+	g++ -c Board.cpp  -std=c++11
+IOstream.o: IOstream.h IOstream.cpp
+	g++ -c IOstream.cpp  -std=c++11
 main.o: main.cpp
-	g++ -c main.cpp
+	g++ -c main.cpp  -std=c++11
 clean:
 	rm *.o output
